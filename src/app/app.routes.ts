587ead31468825cac33b98276b4ef08d91a24e3e
Stephen Buckley <58293351+oimediaDev@users.noreply.github.com>
2023-11-15 11:16:08 +0000

    Refactor the root application to a standalone app

---
+++ src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Route } from '@angular/router';
+import {SaComponentsComponent} from "./sa-components/sa-components.component";
+
+export const appRoutes: Route[] = [{path:'', component:SaComponentsComponent}];
